eedabb60c5ce08cf7ee67b080e17cc17ecc14bc3
Saad Ksioui <saadksioui@gmail.com>
2026-08-24 19:05:34 +0100

    implement loading of inverted index and enhance search logic for improved document retrieval

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -70,61 +70,51 @@ class InvertedIndex:
         with open("cache/docmap.pkl", "wb") as file:
             pickle.dump(self.docmap, file)
 
+    def load(self):
+        with open("cache/index.pkl", "rb") as file:
+            self.index = pickle.load(file)
 
-def remove_stopwords(tokens: list[str]) -> list[str]:
-    stemmer = PorterStemmer()
-    with open("data/stopwords.txt", "r") as file:
-        stopwords = set(file.read().splitlines())
-
-    return [stemmer.stem(token) for token in tokens if token not in stopwords]
-
-
-def check_common(query: str, title: str) -> bool:
-    table = str.maketrans("", "", string.punctuation)
-    query_tokens = remove_stopwords(
-        [token for token in query.lower().translate(table).split() if token]
-    )
-    title_tokens = remove_stopwords(
-        [token for token in title.lower().translate(table).split() if token]
-    )
-
-    if not query_tokens or not title_tokens:
-        return False
-
-    query_words = set(query_tokens)
-    title_words = set(title_tokens)
-
-    return any(
-        q_tok in t_tok
-        for q_tok in query_words
-        for t_tok in title_words
-    )
+        with open("cache/docmap.pkl", "rb") as file:
+            self.docmap = pickle.load(file)
 
 
 def search_movies(query: str) -> None:
-    with open("data/movies.json", "r") as file:
-        movies = json.load(file)
+    inverted_index = InvertedIndex()
 
-    results = [
-        movie["title"]
-        for k in movies.values()
-        for movie in k
-        if check_common(query, movie["title"])
-    ]
+    try:
+        inverted_index.load()
+    except FileNotFoundError:
+        print("Error: index has not been built yet.")
+        return
+
+    tokens = tokenize_text(query)
+    results = []
+
+    for token in tokens:
+        documents = inverted_index.get_documents(token)
+
+        for doc_id in documents:
+            if doc_id not in results:
+                results.append(doc_id)
+
+            if len(results) == 5:
+                break
+
+        if len(results) == 5:
+            break
 
     if results:
         print(f"Searching for: {query}")
-        for index, movie in enumerate(results, start=1):
-            print(f"{index}. {movie}")
+
+        for doc_id in results:
+            movie = inverted_index.docmap[doc_id]
+            print(f"{movie['title']} ({doc_id})")
 
 
 def build_command() -> None:
     inverted_index = InvertedIndex()
     inverted_index.build()
     inverted_index.save()
-
-    docs = inverted_index.get_documents("merida")
-    print(f"First document for token 'merida' = {docs[0]}")
 
 
 def main() -> None:
