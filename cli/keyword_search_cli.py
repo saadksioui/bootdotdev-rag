9ee9646abf30714f4d1e695e1b70decac523b28a
Saad Ksioui <saadksioui@gmail.com>
2026-09-08 13:32:09 +0100

    enhance inverted index with term frequency tracking and update search functionality

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,31 +3,48 @@ import argparse
 import string
 import pickle
 import os
+import math
 from nltk.stem import PorterStemmer
+from collections import Counter
 
 class InvertedIndex:
     def __init__(self):
         self.index = {}
         self.docmap = {}
+        self.term_frequencies = {}
 
     def __add_document(self, doc_id, text):
         tokens = clean_punctuation_stopwords(text)
-        self.docmap[doc_id] = text
 
-        for token in tokens:
+        self.term_frequencies[doc_id] = Counter(tokens)
+        for token in set(tokens):
             if token not in self.index:
                 self.index[token] = []
             self.index[token].append(doc_id)
 
+    def _tokenizer(self, term):
+        tokens = clean_punctuation_stopwords(term)
+        return tokens
 
     def get_documents(self, term):
         doc_ids = self.index.get(term, [])
         return sorted(doc_ids)
 
+    def get_tf(self, doc_id, term):
+        tokens = self._tokenizer(term)
+        if len(tokens) != 1:
+            raise ValueError("Term must contain exactly one searchable token")
+        token = tokens[0]
+        if doc_id not in self.term_frequencies:
+            return 0
+        return self.term_frequencies[doc_id].get(token, 0)
+
     def build(self):
         movies = load_file("data/movies.json")
         for m in movies:
-            self.__add_document(m['id'], f"{m['title']} {m['description']}")
+            doc_id = m["id"]
+            self.docmap[doc_id] = m
+            self.__add_document(doc_id, f"{m['title']} {m['description']}")
 
     def save(self):
         if not os.path.exists("cache"):
@@ -36,14 +53,26 @@ class InvertedIndex:
             pickle.dump(self.index, f)
         with open("cache/docmap.pkl", "wb") as f:
             pickle.dump(self.docmap, f)
+        with open("cache/term_frequencies.pkl", "wb") as f:
+            pickle.dump(self.term_frequencies, f)
 
+    def load(self):
+        try:
+            with open("cache/index.pkl", "rb") as file:
+                self.index = pickle.load(file)
 
-def build_command():
-    index = InvertedIndex()
-    index.build()
-    index.save()
-    docs = index.get_documents('merida')
-    print(f"First document for token 'merida' = {docs[0]}")
+            with open("cache/docmap.pkl", "rb") as file:
+                self.docmap = pickle.load(file)
+
+            with open("cache/term_frequencies.pkl", "rb") as file:
+                self.term_frequencies = pickle.load(file)
+            return True
+        except FileNotFoundError:
+            return None
+
+def build_command(inverted):
+    inverted.build()
+    inverted.save()
 
 def stemming(tokens):
     stemmer = PorterStemmer()
@@ -84,32 +113,60 @@ def load_file(file_path):
     return movies
 
 
-def search_movies(query):
-    movies = load_file("data/movies.json")
-    search_cleaned = clean_punctuation_stopwords(query)
+def search_movies(query, inverted):
+    if inverted.load() is None:
+        print("Files don't exist. Run build first.")
+        return
+    tokens = clean_punctuation_stopwords(query)
     found_movies = []
-    for movie in movies:
-        title_cleaned = clean_punctuation_stopwords(movie['title'])
-        if check_common(search_cleaned, title_cleaned):
-            found_movies.append(movie)
-    for i, movie in enumerate(found_movies, start=1):
-        print(f"{i}: {movie['title']}")
+    limit = 5
+    for token in tokens:
+        if token in inverted.index:
+            for doc_id in inverted.index[token]:
+                found_movies.append(doc_id)
+                if len(found_movies) >= limit:
+                    break
+        if len(found_movies) >= limit:
+            break
+    for doc_id in found_movies:
+        movie = inverted.docmap[doc_id]
+        print(f"{movie['title']} ({doc_id})")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    subparsers.add_parser("build", help="Build the inverted index and store in the disk")
+    term_freq = subparsers.add_parser("tf", help="Build the inverted index and store in the disk")
+    inv_doc_freq = subparsers.add_parser("idf", help="get the freq of a word that are specific to a given dataset")
+    inv_doc_freq.add_argument("term", type=str, help="Term")
+    term_freq.add_argument("doc_id", type=int, help="Document ID")
+    term_freq.add_argument("term", type=str, help="Term")
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
-    build_parser = subparsers.add_parser("build", help="Build the inverted index and store in the disk")
     search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
+    inverted = InvertedIndex()
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            search_movies(args.query)
+            search_movies(args.query, inverted)
         case "build":
-            build_command()
+            build_command(inverted)
+        case "tf":
+            freq = inverted.get_tf(args.doc_id, args.term)
+            print(freq)
+        case "idf":
+            if inverted.load() is None:
+                print("Files don't exist. Run build first.")
+                return
+            tokens = inverted._tokenizer(args.term)
+            total_doc_count = len(inverted.docmap)
+            term_match_doc_count = 0
+            for token in tokens:
+                term_match_doc_count += len(inverted.get_documents(token)) 
+            idf = math.log((total_doc_count + 1) / (term_match_doc_count + 1))
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
         case _:
             parser.print_help()
 
